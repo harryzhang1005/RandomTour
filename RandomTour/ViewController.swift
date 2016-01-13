5fa473415b3f901ed2dfa505323b268f5735ebc0
Harvey <zhanghfglad@gmail.com>
2016-01-13 10:14:14 -0500

    Initial Commit

---
+++ RandomTour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomTour
+//
+//  Created by Harvey Zhang on 1/13/16.
+//  Copyright © 2016 HappyGuy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
